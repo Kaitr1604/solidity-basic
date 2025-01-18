2b478394c16d99f7a95fccd35a5c3c9cbbf0524f
Kaitr1604 <kai.trh1604@gmail.com>
2025-01-18 11:17:44 +0700

    control conditions

---
+++ contracts/7_controlcondition.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: None
+pragma solidity 0.8.3;
+
+contract Function {
+
+    //default scope: private
+    bool isEnabled; //true / false
+    uint a; // 1,3,4
+    address sender;
+    bytes32 data; //
+
+    function setValue(uint _value) external {
+        if (isEnabled == true && a == 1) {
+            //do something
+            a = 2;
+            while (isEnabled) {
+                if (a == 5) {
+                    isEnabled = false;
+                }
+                //do something
+            }
+            // do something
+        } else {
+            for (uint i = 0; i < 10; i++) {
+                //do something
+                if (i == 9) {
+                    break;
+                }
+                if (i == 5) {
+                    continue ;
+                }
+            }
+        }
+    }
+}
