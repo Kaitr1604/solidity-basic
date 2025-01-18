6b3ffea282e422ee3755212837511b542dcecd4e
Kaitr1604 <kai.trh1604@gmail.com>
2025-01-18 15:51:12 +0700

    debug

--- contracts/a_13_debug.sol
+++ contracts/a_13_debug.sol
@@ -1,15 +1,46 @@
 // SPDX-License-Identifier: None
 pragma solidity 0.8.3;
 
-contract SendAndReceiveETH {
-    event deposit (address from, uint amount);
+contract DEBUG {
+    //require
+    //modifier
+    uint public a = 10;
+    uint public b = 100;
     
-    function sendEther(address payable receiver, uint _amount) external  {
-        receiver.transfer(_amount);
+    function foo() public  {
+        a = 5;
+        require(a == 8, "a need to be 8");
     }
 
-    function buyEther () external  payable  {
-        msg.value;
-        address(this).balance;
+    modifier checkA() {
+        require(a == 8, "a need to be 8");
+        _;
+    }
+
+    modifier checkB(uint _testvalue) {
+        require(b == _testvalue, "b need to match value");
+        _;
+    }
+
+    function bar() public checkA checkB(9)  {
+        a = 5;
+    }
+
+    function test() public checkB(10)  {
+        a = 5;
+    }
+
+    function test2(uint _v) public checkB(_v) {
+        a = 5;
+    }
+
+    address admin = 0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B;
+    modifier  onlyAdmin() {
+        require(msg.sender == admin, "only admin can call");
+        _;
+    }
+
+    function testModifier() public onlyAdmin {
+        b = 1000;
     }
 }
